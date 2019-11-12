b044c19d02a9fc1b7427fa210514ba8aaa9da253
vishwasta <42470466+vishwasta@users.noreply.github.com>
2019-11-12 22:36:25 +0530

    5th program

---
+++ 5.R
@@ -0,0 +1,16 @@
+library(visualize)
+library(BSDA)
+rural<-c(3.1,2.9,2.8,3.0,2.7,3.1,2.6,2.8,2.9,3.0)
+urban<-c(3.5,3.0,3.1,3.2,2.9,3.4,3.0,3.4,2.8,3.4)
+xrbar=mean(rural)
+xrbar
+xurbar=mean(urban)
+xurbar
+var(rural)
+sd(rural)
+var(urban)
+sd(urban)
+t.test(x=rural,y=urban,var.equal = TRUE,conf.level = 0.95)
+qt(p=0.05/2,df=18,lower.tail = FALSE)
+visualize.t(stat=c(-2.9886,2.9886),df=18,section="tails")
+visualize.t(stat=c(-2.100922,2.100922),df=18,section="tails")
